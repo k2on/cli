09ddf7742aae5576a3d3e3b0ba16d688be85d1c6
Max Koon <theakman96@gmail.com>
2024-10-26 13:56:25 -0400

    feat: add function input

--- examples/basic/src/index.ts
+++ examples/basic/src/index.ts
@@ -1,10 +1,22 @@
 import { run, router, command } from "@koons/cli";
+import z from "zod";
 
 const cli = router({
     add: router({
-        module: command().fn(() => {
-            console.log("Adding module");
-        }),
+        module: command()
+            .input(z.object({ name: z.number() }))
+            .fn(({ name }) => {
+                console.log(`Name: ${name}`);
+            }),
+        package: command()
+            .input(
+                z.object({
+                    name: z.string(),
+                }),
+            )
+            .fn(({ name }) => {
+                console.log(`Package name: ${name}`);
+            }),
     }),
     help: command().fn(() => console.log("Help command")),
     version: command().fn(() => console.log("Version 1.0")),
